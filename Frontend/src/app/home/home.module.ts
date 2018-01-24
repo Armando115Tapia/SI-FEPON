40ed6eb890f09f4ac7e64087529830b352bf3c8b
luis <lomejordejr@riseup.net>
2018-01-24 11:57:04 -0500

    Cambios en idioma de interfaz

--- Frontend/src/app/home/home.module.ts
+++ Frontend/src/app/home/home.module.ts
@@ -6,7 +6,6 @@ import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { QuoteService } from './quote.service';
 
 @NgModule({
   imports: [
@@ -19,8 +18,6 @@ import { QuoteService } from './quote.service';
   declarations: [
     HomeComponent
   ],
-  providers: [
-    QuoteService
-  ]
+  providers: []
 })
 export class HomeModule { }
